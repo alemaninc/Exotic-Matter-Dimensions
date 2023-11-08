a55c1523b9b5ab64a68a8d06855d5f631a1fdf63
alemaninc <97172850+alemaninc@users.noreply.github.com>
2023-11-08 18:40:23 +0000

    Delete upgrades.js

--- upgrades.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/*
-Everything that is represented in "g" as an object generated from a list of keys is stored here and executed directly before main.js.
-*/
-const starList = countTo(10).map(x=>countTo(4).map(y=>x*10+y)).flat()
