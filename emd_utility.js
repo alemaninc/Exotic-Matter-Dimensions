a50770b127c6c39812d8a04c026eac47de25bf51
alemaninc <97172850+alemaninc@users.noreply.github.com>
2024-06-03 11:51:34 +0100

    1.5(c).12.1.1

--- emd_utility.js
+++ emd_utility.js
@@ -1,7 +1,7 @@
 "use strict";
 var initComplete = false
 const version = {
-	current:"𝕍1.5(c).12",
+	current:"𝕍1.5(c).12.1",
 	nextPercentage:function(x=version.nextProgress){return (typeof x === "number")?x:(x.map(i=>version.nextPercentage(i)).sum()/x.length)},
 	percentage:function(){return "["+(this.nextPercentage()*100).toFixed(0)+"%]"},
 	nextProgress:[1],
